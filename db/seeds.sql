16da6fbb7af93aa81258980b5a75eafaccfb36b3
Diego Cortes <diegocortes4@gmail.com>
2020-12-21 15:51:41 -0500

    setup burger app, set up mysql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Cheese Burger');
+INSERT INTO burger (name) VALUES ('Hamburger');
+INSERT INTO burger (name, Devoured) VALUES ('Mushroom and Swiss', true);
+INSERT INTO burger (name, Devoured) VALUES ('Butter Burger', true);
+INSERT INTO burger (name, Devoured) VALUES ('Grilled Chesse', true);
+INSERT INTO burger (name) VALUES ('Vegie Burger');
